1a811bf3206cb35aa22b5d5e43663d08d035a611
Kartik Pant <kartikpant.kp69@gmail.com>
2025-07-21 08:33:24 +0530

    More on ES6

---
+++ BASIC/ES(ECMASCRIPT_6)/ES_6.js
@@ -0,0 +1,47 @@
+// ecma script or ES 6 included many features 
+
+
+// 1. let 
+// 2. const 
+// 3 . Arrow fucntion 
+
+// let is used for the declaration of variable 
+// it has universal scope inspite to the {}
+// and let also can be redefined 
+
+// const is used for the declaration of variable whose values doesnot change
+
+//  Template literals or template strings 
+// its a way to write a data 
+
+let firstName ='kartik';
+let lastName = 'pant';
+console.log(`${firstName} ${lastName}`);
+
+
+//  string interpolation 
+//  it is also a way to write a data using template literals 
+let age =16;
+let message =`I am ${age} years old `;
+console.log(message );
+
+
+
+//  Arrow function 
+const sum = function( a, b ){
+    let result = `The sum of ${a} and ${b} is ${a+b}`;
+        console.log(result);
+
+} 
+sum(5,10);        // this is the traditionl method to declare a function 
+
+
+
+// using fat arrow function 
+const sum1 =(a,b) => {
+              let result = `The sum of ${a} and ${b} is ${a+b}`;
+        console.log(result);
+
+};
+
+sum1(5,10);
---
+++ BASIC/ES(ECMASCRIPT_6)/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+
+
+// using fat arrow function 
+const sum1 =(a,b) => {
+              let result = `The sum of ${a} and ${b} is ${a+b}`;
+        console.log(result);
+
+};
+
+sum1(5,10);
