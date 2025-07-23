9e4c479f8489f9529a7a6b978caa63fbec60ab2d
Kartik Pant <kartikpant.kp69@gmail.com>
2025-07-23 21:08:22 +0530

    dom

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,14 +1,63 @@
-// console.log(document.body.childNodes);
-
-// const { createElement } = require("react");
-
-//  
-
+// creating a tag using js
+const para=document.createElement("p");
+para.innerText ="hello world";
+document.body.appendChild(para); // this show the change in browser
 
 
-// ///// accesing the element by using getelementbyid()
-let a = document.getElementById("first");
-let b= document.getElementById("second");
-console.log(a);
-console.log(b);
-// let parent;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // console.log(document.body.childNodes);
+
+// // const { createElement } = require("react");
+
+// //  
+
+
+
+// // ///// accesing the element by using getelementbyid()
+// // let a = document.getElementById("first");
+// // let b= document.getElementById("second");
+// // console.log(a);
+// // console.log(b);
+
+
+// // using query selector
+// let c = document.querySelector(".first");
+// // let parent;
+// console.log(c);
+
+
+// // for updating the code 
+
+// // 1. using innerHTML 
+// // 2. using outterHTML
+// // 3. using text-content
+// // 4. using inneText
+
+
+
+// //  creating a button using js and modifying it 
+
+
+
+// let button=document.createElement("button");
+// // console.log(button);
+// //  now inserting the data inside the button
+// // document.button.innerHTML("<p>hello world <\p>");
